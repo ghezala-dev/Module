d2f372863c62b147ea653ea323476ea95bd1cccc
ghezala <ghezalen@gmail.com>
2022-09-23 00:32:08 +0430

    commit

---
+++ Module #5/index3.js
@@ -0,0 +1,16 @@
+const names = ['Karl', 'John', 'Steve'];
+const people = [{ name: 'Karl', location: 'UK' }, { name: 'Steve', location: 'US' }];
+
+/
+for (let i = 0; i < names.length; i++) {
+}
+for (const name of names) {
+	}
+	for (let i = 0; i < people.length; i++) {
+	}
+	}
+
+for (const person of people) {
+    console.log(person.name);
+    console.log(person.location);
+}
